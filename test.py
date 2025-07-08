65ecd4efedf00d5bca3ab863947d05271e748117
shardy1x <120123379+Manveen07@users.noreply.github.com>
2025-07-08 17:29:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,6 @@ data = {
     "client_secret": client_secret,
     "grant_type": "client_credentials"
 }
-access_token = 'GP8apsXTdNnxqgxKDlK9NMGBZ5tL0pgIGqDDYzIJ1ug'
 headers = {
     "Authorization": f"Bearer {access_token}",
     "Content-Type": "application/json"
